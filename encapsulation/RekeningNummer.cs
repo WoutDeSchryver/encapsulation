bf4f1882a72529e598e56ad47919583c04f2b8d4
WoutDeSchryver <144115634+WoutDeSchryver@users.noreply.github.com>
2024-03-13 22:44:57 +0100

    Update RekeningNummer.cs

--- encapsulation/RekeningNummer.cs
+++ encapsulation/RekeningNummer.cs
@@ -1,13 +1,22 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace encapsulation
 {
-    public class RekeningNummer
+    private readonly int protocolNummer
+    private readonly int echtRekeningNummer
+    private readonly int controleNummer
+    public readonly string rekeningNummer 
+
+    public class RekeningNummer(int protocolNummer, int rekeningNummer)
     {
-      
+        this.protocolNummer = protocolNummer
+        this.echtRekeningNummer = rekeningNummer
+        this.controleNummer = (protocolNummer + rekeningNummer) % 97
+        this.rekeningNummer = $"BE{protocolNummer}{echtRekeningNummer}{controleNummer}"
     }
 }
